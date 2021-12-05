693dba9cc68d4462050d47d0cb6e4c7f219b6ac4
CosmoAndrade <cosmo.andrade@hotmail.com>
2021-12-05 11:01:29 -0300

    Criando a função de criar login no index

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,34 @@
-document.getElementById('link-conta').addEventListener('click', function(){
-    alert('Clicou')
-})
+// CRIAR CONTA
+const myModal = new bootstrap.Modal('#register-modal'); // Fechar modal do bootstrap
+
+document.getElementById('create-form').addEventListener('submit', function(e){
+  e.preventDefault();
+
+  const email =  document.getElementById('email-create-input').value;
+  const password = document.getElementById('password-create-input').value;
+
+  if (email.length < 5){
+      alert('Preencha com um email válido!');
+      return;
+  };
+
+  if (password.length < 4){
+    alert('Preencha a senha com no mínimo 4 digitos!');
+    return;
+   
+};
+
+    saveAccount({
+        login: email,
+        password : password,
+        transactions : []
+    });
+
+myModal.hide();
+alert('Conta criada com sucesso!')
+ 
+});
+
+function saveAccount(data){
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
