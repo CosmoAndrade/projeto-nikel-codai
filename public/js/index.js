6bb85a4c1c6cb91950ac56f189046d0047e97b44
CosmoAndrade <cosmo.andrade@hotmail.com>
2021-12-05 16:16:37 -0300

    Refatorando index

--- public/js/index.js
+++ public/js/index.js
@@ -1,109 +1,90 @@
-const myModal = new bootstrap.Modal('#register-modal'); // Fechar modal do bootstrap
-let logged = sessionStorage.getItem('logged');
-const session = localStorage.getItem('session');
+const myModal = new bootstrap.Modal("#register-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
 
 checkLogged();
 
-
-//LOGAR SISTEMA
-
-document.getElementById('login-form').addEventListener('submit' , function(e){
-        e.preventDefault();
-
-        
-  const email =  document.getElementById('email-input').value;
-  const password = document.getElementById('password-input').value;
-  const check_session = document.getElementById('session-check').checked;
+//LOGAR NO SISTEMA
+document.getElementById("login-form").addEventListener('submit', function(e) {
+    e.preventDefault();
+    const email = document.getElementById('email-input').value;
+    const password = document.getElementById('password-input').value;
+    const checkSession = document.getElementById('session-check').checked;
 
     const account = getAccount(email);
 
-    if (!account){
-        alert('Oppss! Verifique o usuário ou a senha!');
+    if(!account) {
+        alert("Opps! Verifique o usuário ou sua senha.");
         return;
-    };
+    }
 
-    if (account){
-        if(account.password !== password){
-            alert('Oppss! Verifique o usuário ou a senha!');
-        return;
-        };
+    if(account) {
+        if(account.password !== password) {
+            alert("Opps! Verifique o usuário ou sua senha.");
+            return;
+        }
 
-       saveSession(email, check_session);
-
-        
+        saveSession(email, checkSession);
 
         window.location.href = 'home.html';
-    };
-
-  
+    }
 });
-function checkLogged(){
-    if(session){
-        sessionStorage.setItem('logged',session);
+
+//CRIAR CONTA
+document.getElementById("create-form").addEventListener('submit', function(e) {
+    e.preventDefault();
+    const email = document.getElementById('email-create-input').value;
+    const password = document.getElementById('password-create-input').value;
+
+    if(email.length < 3) {
+        alert("Preencha o campo com um e-mail válido.");
+        return;
+    }
+
+    if(password.length < 4) {
+        alert("Preencha a senha com no mínimo 4 digitos.")
+        return;
+    }
+
+    saveAccount({login: email, password: password, transactions: []});
+
+    myModal.hide();
+
+    alert("Conta criada com sucesso.");
+
+});
+
+function checkLogged() {
+    if(session) {
+        sessionStorage.setItem("logged", session);
         logged = session;
-    };
+    }
 
-    if(logged){
-        saveSession(logged,session);
+    if(logged) {
+        saveSession(logged, session);
         window.location.href = 'home.html';
-    };
-};
+    }
 
-function saveSession(data, saveSession){
-        if(saveSession){
-            localStorage.setItem('session',data);
-        };
+}
 
-        sessionStorage.setItem('logged',data);
-      
-};
+function saveAccount(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
 
+function saveSession(data, saveSession) {
+    if(saveSession) {
+        localStorage.setItem("session", data);
+    }
 
-function getAccount(key){
+    sessionStorage.setItem("logged", data);
+}
+
+function getAccount(key) {
     const account = localStorage.getItem(key);
 
-    if (account){
+    if(account) {
         return JSON.parse(account);
     }
 
-    return '';
-}
-
-
-
-
-
-// CRIAR CONTA
-
-
-document.getElementById('create-form').addEventListener('submit', function(e){
-  e.preventDefault();
-
-  const email =  document.getElementById('email-create-input').value;
-  const password = document.getElementById('password-create-input').value;
-
-  if (email.length < 5){
-      alert('Preencha com um email válido!');
-      return;
-  };
-
-  if (password.length < 4){
-    alert('Preencha a senha com no mínimo 4 digitos!');
-    return;
-   
-};
-
-    saveAccount({
-        login: email,
-        password : password,
-        transactions : []
-    });
-
-myModal.hide();
-alert('Conta criada com sucesso!')
- 
-});
-
-function saveAccount(data){
-    localStorage.setItem(data.login, JSON.stringify(data));
+    return "";
 }
